147d31eef8042a97d6c1dde255d1f873b0fadff2
Ted Bui <tedtbui@gmail.com>
2021-01-05 22:12:22 -0800

    Removed extra stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+// Components
+
+// CSS
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Descent 2E Party Tracker</h1>
+
+      {/* Character Tracker  */}
+      
+      {/* Character Selector */}
     </div>
   );
 }
