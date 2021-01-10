cd8cc406b8493c4df91c5419b5a3333dcf58d984
Ted Bui <tedtbui@gmail.com>
2021-01-10 00:57:52 -0800

    Started Player Profile component

---
+++ src/components/TrackPlayers/PlayerProfile/PlayerProfile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Components
+
+
+const PlayerProfile = ({player}) => {
+    
+
+    return (
+        <div className="row">
+            <div className="c">
+
+            </div>
+        </div>
+    );
+}
+
+export default PlayerProfile;
